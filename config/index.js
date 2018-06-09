ee948c8d77e7a37e33fa5426347416dee22d7d1a
abhishekwl <nateriver210@gmail.com>
2018-06-09 14:24:36 +0530

    Added firebase

--- config/index.js
+++ config/index.js
@@ -1,5 +1,13 @@
 module.exports = {
     apiUri: "/api/v1",
-    dbUri: "mongodb://localhost:27017/stem_new",
-    port: process.env.PORT || 8000
+    dbUri: "mongodb://abhishekwl:abcd1234@ds016108.mlab.com:16108/stem_api",
+    port: process.env.PORT || 8000,
+    firebaseConfig: {
+        apiKey: "AIzaSyDG7gSPLRVSbnOiJ27_fKlcafY3xbN5Zmo",
+        authDomain: "b-t-prototype.firebaseapp.com",
+        databaseURL: "https://b-t-prototype.firebaseio.com",
+        projectId: "b-t-prototype",
+        storageBucket: "b-t-prototype.appspot.com",
+        messagingSenderId: "430139558882"
+    }
 };
--- index.js
+++ index.js
@@ -2,17 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
+const firebase = require("firebase");
 //LOCAL
 const config = require("./config");
-
-mongoose.Promise = global.Promise;
-mongoose.connect(config.dbUri).then(()=>{
-    console.log("[DB] Hook to DB success");
-}).catch(err=>{
-    console.log("[!ERROR-DB] "+err);
-    process.exit();
-});
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -22,5 +14,18 @@ app.get("/", (req,res)=>{
     res.status(200).json({ message: "All requests should be directed to /API endpoint." });
 });
 app.listen(config.port, "0.0.0.0", ()=>{
-    console.log("[SERVER] Listening on port "+config.port);
+    connectToDatabase();
+    console.log("\n[SERVER] Listening on port "+config.port);
 });
+
+function connectToDatabase() {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(config.dbUri).then(()=>{
+        firebase.initializeApp(config.firebaseConfig);
+        console.log("[FIREBASE] "+(firebase.apps.length==1?"Hook to Firebase success":"ERROR CONNECTING TO FIREBASE"));
+        console.log("[DB] Hook to DB success\n");
+    }).catch(err=>{
+        console.log("[!ERROR-DB] "+err);
+        process.exit();
+    });
+}
