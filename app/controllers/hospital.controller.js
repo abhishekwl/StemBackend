b37afda82c0ac774ae66e2d154b45f4fc201444c
abhishekwl <nateriver210@gmail.com>
2018-06-10 12:05:09 +0530

    Added debug console logs

--- app/controllers/hospital.controller.js
+++ app/controllers/hospital.controller.js
@@ -3,8 +3,7 @@ const Hospital = require("../models/hospital.model.js");
 exports.create = (req,res)=>{
     if(req.body.uid && req.body.name && req.body.latitude && req.body.longitude && req.body.city) {
         Hospital.find({ uid: req.body.uid }, (err,data)=>{
-            console.log("[DEBUG] "+data);
-            if(data.length) sendData("UID already exists, not creating a new entry.", null, res);
+            if(data.length) sendData("UID already exists, not creating a new entry.", null, req, res);
             else {
                 const hospital = new Hospital({
                     uid: req.body.uid,
@@ -14,17 +13,19 @@ exports.create = (req,res)=>{
                     city: req.body.city,
                     image: req.body.image? req.body.image : ""
                 });
-                hospital.save((err,data)=>sendData(err,data,res));
+                hospital.save((err,data)=>sendData(err,data,req,res));
             }
         }).limit(1);
-    } else sendData("Missing POST body params.", null, res);
+    } else sendData("Missing POST body params.", null, req, res);
 };
 
 exports.findOne = (req,res)=>{
     Hospital.findOne({ uid: req.params.hospitalId }, (err,data)=>sendData(err,data,res));
 };
 
-function sendData(err,data,res) {
+function sendData(err,data,req,res) {
+    const remoteIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log("\n["+req.method+"] "+remoteIp+" "+req.url);
     if(err) {
         console.log("[!ERR-HOSPITAL-CONTROLLER] "+err);
         res.status(400).json({ error: err });
